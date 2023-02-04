267a70e9e21e4f4daf1940658daf0fc6b48c8a63
Manoj-Katkar <satyammanoj2001@gmail.com>
2023-02-04 19:21:13 +0530

    fibonacci code added

--- Recursion.java
+++ Recursion.java
@@ -5,7 +5,11 @@ public class Recursion{
         int N = 5;
         // call the Function
         // name(N);
-        one_To_N(N);
+        // one_To_N(N);
+
+        // calling the fibonacci number function
+        int fib = ntn_fibonacci(N);
+        System.out.println(fib);
         
     }
     /* 
@@ -22,7 +26,7 @@ public class Recursion{
 
     // write the recursive code to print the number in increasing order
     // example = 1 2 3 4 5
-
+/* 
     static void one_To_N(int N){
         if(N == 0){
             return;
@@ -32,4 +36,22 @@ public class Recursion{
         System.out.println(N);
 
     }
+    */
+
+    // write the recursive function to find the N th fibonacci numbers 
+
+    static int ntn_fibonacci(int N){
+        // base case for the n th ibonacci number 
+        if(N == 0 || N == 1){
+            return N;
+        }
+
+        // main logic
+        // find the two previous numbers with the help of same Function
+        int temp_1 = ntn_fibonacci(N - 1);
+        int temp_2 = ntn_fibonacci(N - 2);
+
+        return temp_1 + temp_2;
+    }
+
 }
